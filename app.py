0eb2fe14045dfb66230b2d47c8626183a9a3b376
Alex Jones <ajonzy@gmail.com>
2020-06-30 14:07:19 -0600

    Added endpoints for adding and getting file data

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_heroku import Heroku
 import io
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://xrxkzappyeybvq:c4077974c96b70a2d9dffe88d34b5cdbb19b6cd7a4e811a5635bc30d6a6a2884@ec2-3-216-129-140.compute-1.amazonaws.com:5432/d42e4h2v11jd2o"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -32,6 +32,31 @@ class FileSchema(ma.Schema):
 
 file_schema = FileSchema()
 files_schema = FileSchema(many=True)
+
+
+@app.route("/file/add", methods=["POST"])
+def add_file():
+    name = request.form.get("name")
+    file_type = request.form.get("type")
+    data = request.files.get("data")
+
+    new_file = File(name, file_type, data.read())
+    db.session.add(new_file)
+    db.session.commit()
+
+    return jsonify("File added successfully")
+
+@app.route("/file/get/data", methods=["GET"])
+def get_file_data():
+    file_data = db.session.query(File).all()
+    return jsonify(files_schema.dump(file_data))
+
+@app.route("/file/get/<id>", methods=["GET"])
+def get_file(id):
+    file_data = db.session.query(File).filter(File.id == id).first()
+    return send_file(io.BytesIO(file_data.data), 
+                    attachment_filename=file_data.name, 
+                    mimetype=file_data.file_type)
 
 
 if __name__ == "__main__":
